aeb0851ae15c787872e448f85f9fdfc34448effb
Daniel Loyd <daniel.loyddan@gmail.com>
2019-04-27 11:46:43 -0700

    push newest

--- public/javascript/user/login.js
+++ public/javascript/user/login.js
@@ -1,4 +1,10 @@
 function validateData() {
+	console.log("signing in");
+	if(firebase.auth().currentUser != null)
+	{
+		firebase.auth().signOut(); 
+		console.log("signed out");
+	}
         if (!firebase.auth().currentUser) {
                 // [START createprovider]
                 var provider = new firebase.auth.GoogleAuthProvider();
@@ -8,13 +14,15 @@ function validateData() {
                 // [END addscopes]
                 // [START signin]
                 var signedInUser = firebase.auth().signInWithPopup(provider);
-		console.log(signedInUser);
                 signedInUser.then(function(result) {
                         // This gives you a Google Access Token. You can use it to access the Google API.
                         var token = result.credential.accessToken;
                         // The signed-in user info.
                         var user = result.user;
-                        console.log("User Info\n "+user);
+			console.log("signed in:");
+			console.log(signedInUser);
+			console.log(firebase.auth().currentUser);
+			console.log(firebase.auth().currentUser.email);
                         // [START_EXCLUDE]
                         console.log("GOOGLE TOKEN\n")
                         console.log(token);
@@ -33,5 +41,13 @@ function validateData() {
                 }); 
                 // [END signin]
         }   
+	//check to make sure given gmail is a valid user
+	console.log("ensuring valid user is logged in");
+	var db = firebase.database().ref();
+	var inStr = '/encounterUsers/'+firebase.auth().currentUser.email;
+	console.log("database query:"+inStr);
+	db.child(inStr).once('value').then(function(snapshot) {
+		if(snapshot.val())
+	});
 }
 
